a03b1ddffddd43261e8e2b30b5173c0a4a815d8d
alikansoh <alikanso.dev@gmail.com>
2026-01-03 14:35:57 +0000

    remove the offer modal

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -4,12 +4,10 @@ import About from "./components/About";
 import WhyChooseUs from "./components/WhyChooseUs";
 import HowWeWork from "./components/HowWeWork";
 import Projects from "./components/Projects";
-import OfferModal from "./components/offer"; // Add this
 
 export default function Home() {
   return (
     <main className="">
-      <OfferModal /> {/* Add this at the top */}
       <Hero />
       <About />
       <WhyChooseUs />
